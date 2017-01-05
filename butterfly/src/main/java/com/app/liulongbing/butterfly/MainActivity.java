1a5e1e0bf692723011ca57fd13d9c7bf4ec69988
Aleaves <2849459897@qq.com>
2017-01-05 10:48:44 +0800

    123

--- butterfly/src/main/java/com/app/liulongbing/butterfly/MainActivity.java
+++ butterfly/src/main/java/com/app/liulongbing/butterfly/MainActivity.java
@@ -1,23 +1,13 @@
 package com.app.liulongbing.butterfly;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-
 public class MainActivity extends AppCompatActivity {
 
+
+    private int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = new WebView(this);
-        
+
     }
 
 
